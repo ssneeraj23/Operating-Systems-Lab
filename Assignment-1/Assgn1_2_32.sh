16741e679360d5e52450d3bb9245e1da3283258d
Neeraj Boddeda <101348975+ssneeraj23@users.noreply.github.com>
2023-01-24 20:55:56 +0530

    Challenge me

---
+++ Assignment-1/Assgn1_2_32.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+input=$1
+out="validation_results.txt"
+
+while read -r v; do
+    [ -z "$v" ] && continue
+    temp=1
+    size=${#v}
+    if [ $size -ge 5 ] && [ $size -le 20 ]; then
+        if grep -q '^[A-Za-z][A-Za-z0-9]*[0-9][A-Za-z0-9]*$' <<<$v; then
+            while read -r f; do
+                if grep -qi $f <<<$v; then
+                    temp=0
+                    break
+                fi
+            done <"fruits.txt"
+            if [ $temp -eq 1 ];then echo "YES" >>$out;else echo "NO" >>$out;fi
+        else
+            echo "NO" >>$out
+        fi
+    else
+        echo "NO" >>$out
+    fi
+
+done <"$input"
