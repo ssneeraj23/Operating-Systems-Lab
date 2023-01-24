c6b78d61779030ad0b0809679e8d5895d14c56e2
Neeraj Boddeda <101348975+ssneeraj23@users.noreply.github.com>
2023-01-24 15:12:33 +0530

    Update Assgn1_9_<20CS30019>.sh

--- Assignment-1/Assgn1_9_<20CS30019>.sh
+++ Assignment-1/Assgn1_9_<20CS30019>.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/usr/bin/bash
 unique_names=($(awk '{print $1}' majors.txt | sort -u))
 unique_majors=($(awk '{print $2}' majors.txt | sort -u))
 single_major_count=0
 multiple_major_students=""
 for major in "${unique_majors[@]}"
 do
-    echo "$(grep -c "$major" majors.txt) $major"
-done | sort -k1,1nr -k2,2
+    echo "$major $(grep -c "$major" majors.txt)"
+done | sort -k2,2nr -k1,1
 for name in "${unique_names[@]}"
 do
     if [ $(grep -c "$name" majors.txt) -gt 1 ]; then
