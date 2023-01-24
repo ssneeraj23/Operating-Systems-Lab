cfdbae37046a9efcbb64f1d04b6f067a0ef4b211
fartbox123 <102210443+fartbox123@users.noreply.github.com>
2023-01-25 02:19:24 +0530

    Update Assgn1_3_32.sh

--- Assignment-1/Final_submissions/Assgn1_3_32.sh
+++ Assignment-1/Final_submissions/Assgn1_3_32.sh
@@ -7,8 +7,9 @@ fi
 
 json_dir=$1
 csv_dir=$2
-
+if [ ! -d "$2" ]; then
 mkdir $2
+fi
 
 cd $json_dir
 
@@ -21,9 +22,6 @@ cd ..
 
 shift 2
 
-ls
-
-pwd
 
 list=("$@")
 
@@ -35,7 +33,7 @@ for f in $json_dir/*.json; do
 	cmd="jq -r '.[] | ["
 	for s in $@;
 	do
-		echo $s
+		#echo $s
 		cmd+=".$s, "
 	done
 	cmd=${cmd::-2}
@@ -43,7 +41,7 @@ for f in $json_dir/*.json; do
 	csv="$(pwd)/$csv_dir/$(basename $f .json).csv"
 	filename="$(basename $file .json)"
 	cmd+=" $json_dir/$(basename $f) > $csv"
-	echo "$cmd"
+	#echo "$cmd"
 	for k in "${list[@]}"; do
 		echo "$k," > $csv.tmp
 	done
@@ -57,10 +55,10 @@ for s in $json_dir/*.json; do
 	rm "$json_dir/$(basename $s)"
 done
 
-for s in $json_dir/*.csv.tmp; do
-	rm "$json_dir/$(basename $s)"
-done
+#for s in $json_dir/*.csv.tmp; do
+#	rm "$json_dir/$(basename $s)"
+#done
 
-for s in $json_dir/*.csv; do
-	rm "$json_dir/$(basename $s)"
-done
+#for s in $json_dir/*.csv; do
+#	rm "$json_dir/$(basename $s)"
+#done
