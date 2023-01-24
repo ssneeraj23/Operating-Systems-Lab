9a84a1a1e0178d10245604cd890886f1c549b5db
Neeraj Boddeda <101348975+ssneeraj23@users.noreply.github.com>
2023-01-25 01:13:32 +0530

    working

--- Assignment-1/Final_submissions/Assgn1_2_32.sh
+++ Assignment-1/Final_submissions/Assgn1_2_32.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/bash
-out="validation_results.txt"
 while read -r v; do
     [ -z "$v" ] && continue
-    temp=0;
-    if [ ${#v} -ge 5 ] && [ ${#v} -le 20 ]; then
-        if grep -q '^[A-Za-z][A-Za-z0-9]*[0-9][A-Za-z0-9]*$' <<<$v; then
-            while read -r f; do
-                if grep -qi $f <<<$v; then
-                    temp=1;break
-                fi
-            done <"fruits.txt"
-        else
-            temp=2
-        fi
+    temp=0
+    if [ ${#v} -ge 5 ] && [ ${#v} -le 20 ] && grep -q '^[A-Za-z][A-Za-z0-9]*[0-9][A-Za-z0-9]*$' <<<$v; then
+        while read -r f; do
+            if grep -qi $f <<<$v; then
+                temp=1
+                break
+            fi
+        done <"fruits.txt"
     else
         temp=2
     fi
-    if [ $temp -eq 0 ];then echo "YES" >>$out;else echo "NO" >>$out; fi
+    if [ $temp -eq 0 ]; then echo "YES" >>"validation_results.txt"; else echo "NO" >>"validation_results.txt"; fi
 done <"$1"
