cc07baafd3be596f49346f387c1e71d8d1ad282a
Neeraj Boddeda <101348975+ssneeraj23@users.noreply.github.com>
2023-03-10 18:32:35 +0530

    Add files via upload

--- Assignment-4/sns.cpp
+++ Assignment-4/sns.cpp
@@ -16,6 +16,10 @@ using namespace std;
 pthread_mutex_t mutex_log = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_monitor = PTHREAD_MUTEX_INITIALIZER;
 
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER; // monitor queue pushing
+
 typedef struct action
 {
     int user_id;
@@ -49,9 +53,9 @@ typedef struct node
     queue<action> wall;
     priority_queue<action, vector<action>, cmp> feed;
     int priority; // 0 indicates chrono
-    int like_cnt=1;
-    int post_cnt=1;
-    int comment_cnt=1;
+    int like_cnt = 1;
+    int post_cnt = 1;
+    int comment_cnt = 1;
 
 } node;
 
@@ -89,7 +93,48 @@ int get_num_comm_neigh(int a, int b, vector<vector<int>> &g)
         swap(a, b);
     return g[a][b - a];
 }
+// pushUpdate threads
+void* pushUpdate(void *arg)
+{
+    cout<<"inside pushupdate\n";
+    while (true)
+    {
+        action i;
+        int flag = 0; // to check if somehting has been popped out of monitor queue
+        // push action of a node into all its neighbours
+        pthread_mutex_lock(&mutex2);
+        pthread_cond_wait(&cond, &mutex2); // to signal that monitor queue has been updated
 
+        pthread_mutex_lock(&mutex_monitor); // to make the pop operation atomic
+        if (!monitor.empty())
+        {
+            flag=1; // indicates that something has been popped out of monitor queue
+            i = monitor.front();
+            monitor.pop();
+        }
+        pthread_mutex_unlock(&mutex_monitor);
+
+        pthread_mutex_lock(&mutex1); // to make adding actions into the feed_queue of neighbours atomic
+        if (flag == 1)
+        {
+            action temp = i;
+            for (auto j : adj[i.user_id])
+            {
+                // if node of id i.user_id has priority bit 1, then chrono_compare else priority_compare
+                all_nodes[j].feed.push(temp);
+                
+                printf("Pushed action %d of user %d into feed queue of user %d\n", temp.action_id, temp.user_id, j);
+                fprintf(log_file, "Pushed action %d of user %d into feed queue of user %d\n", temp.action_id, temp.user_id, j);
+            }
+        }
+        // if time priority we can immediately signal else wait for all actions of a particular node to be pushed into feed queues
+
+        pthread_mutex_unlock(&mutex1);
+
+        pthread_mutex_unlock(&mutex2);
+    }
+    return NULL;
+}
 // User simulator threads
 
 void *user_sim(void *arg)
@@ -120,16 +165,18 @@ void *user_sim(void *arg)
         for (auto x : generated)
         {
             int actions;
-            if(adj[x].size()==0)actions=1;
-            else actions = (int)(10.0 * (1.0 + log2(adj[x].size())));
-            fprintf(log_file, "NodeID : %d   No of Actions generated : %d  Degree : %d\n",x,actions,(int)adj[x].size());
-            fprintf(log_file,"The actions generated are \n");
-            printf("NodeID : %d   No of Actions generated : %d  Degree : %d\n",x,actions,(int)adj[x].size());
-            //printf("The actions generated are \n");
-            int id1,id2,id3;
-            id1=all_nodes[x].like_cnt;
-            id2=all_nodes[x].post_cnt;
-            id3=all_nodes[x].comment_cnt;
+            if (adj[x].size() == 0)
+                actions = 1;
+            else
+                actions = (int)(10.0 * (1.0 + log2(adj[x].size())));
+            fprintf(log_file, "NodeID : %d   No of Actions generated : %d  Degree : %d\n", x, actions, (int)adj[x].size());
+            fprintf(log_file, "The actions generated are \n");
+            printf("NodeID : %d   No of Actions generated : %d  Degree : %d\n", x, actions, (int)adj[x].size());
+            // printf("The actions generated are \n");
+            int id1, id2, id3;
+            id1 = all_nodes[x].like_cnt;
+            id2 = all_nodes[x].post_cnt;
+            id3 = all_nodes[x].comment_cnt;
             string atype;
             for (int i = 0; i < actions; i++)
             {
@@ -137,42 +184,44 @@ void *user_sim(void *arg)
                 temp.user_id = x;
                 temp.action_type = rand() % 3;
                 temp.timestamp = time(NULL);
-                if(temp.action_type==0)
+                if (temp.action_type == 0)
                 {
                     temp.action_id = id1;
                     ++id1;
-                    atype="like";
+                    atype = "like";
                 }
-                else{
-                    if(temp.action_type==1)
+                else
+                {
+                    if (temp.action_type == 1)
                     {
                         temp.action_id = id2;
                         ++id2;
-                        atype="post";
+                        atype = "post";
                     }
                     else
                     {
                         temp.action_id = id3;
                         ++id3;
-                        atype="comment";
+                        atype = "comment";
                     }
                 }
                 // lock the file
                 pthread_mutex_lock(&mutex_log);
-                fprintf(log_file, "UserID: %d action_id: %d action_type: %s timestamp: %s",x,temp.action_id,atype.c_str(),ctime(&temp.timestamp));
-                //printf("UserID: %d action_id: %d action_type: %s timestamp: ",x,temp.action_id,atype,ctime(&temp.timestamp));
+                fprintf(log_file, "UserID: %d action_id: %d action_type: %s timestamp: %s", x, temp.action_id, atype.c_str(), ctime(&temp.timestamp));
+                // printf("UserID: %d action_id: %d action_type: %s timestamp: ",x,temp.action_id,atype,ctime(&temp.timestamp));
                 pthread_mutex_unlock(&mutex_log);
 
                 // lock
                 pthread_mutex_lock(&mutex_monitor);
                 monitor.push(temp);
+                pthread_cond_broadcast(&cond);
                 pthread_mutex_unlock(&mutex_monitor);
 
                 all_nodes[x].wall.push(temp);
             }
-            all_nodes[x].like_cnt=id1;
-            all_nodes[x].post_cnt=id2;
-            all_nodes[x].comment_cnt=id3;
+            all_nodes[x].like_cnt = id1;
+            all_nodes[x].post_cnt = id2;
+            all_nodes[x].comment_cnt = id3;
         }
         sleep(120);
     }
@@ -222,7 +271,21 @@ int main()
 
     // Create thread 1
     pthread_create(&us_sim_thrd, NULL, user_sim, NULL);
+    // creating 25 threads of pushUpdate
+    pthread_t push_update[25];
+    for (int i = 0; i < 25; i++)
+    {
+        pthread_create(push_update, NULL, pushUpdate, NULL);
+    }
 
-    pthread_join(us_sim_thrd, NULL);
+
+
+
+    //To be at the end
+    for (int i = 0; i < 25; i++)
+    {
+        pthread_join(push_update[i],NULL);
+    }
+     pthread_join(us_sim_thrd, NULL);
     return 0;
 }
