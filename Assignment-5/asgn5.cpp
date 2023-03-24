182592aceb3cf6a414d60c1bd6eac39fefe7ad61
MohanKishore369 <98636337+MohanKishore369@users.noreply.github.com>
2023-03-24 16:14:18 +0530

    efr

--- Assignment-5/asgn5.cpp
+++ Assignment-5/asgn5.cpp
@@ -174,6 +174,7 @@ void *cleaner(void *arg)
     {
         pthread_mutex_lock(&dmut);
         pthread_cond_wait(&clean_start, &dmut);
+        pthread_mutex_unlock(&dmut);
         while (1)
         {
             r = rand() % N;
@@ -209,14 +210,32 @@ void *cleaner(void *arg)
                     }
                     // get lock for uncleaned
                     pthread_mutex_lock(&unclnlock);
-                    if(uncleaned == 0) {
+                    if (uncleaned == 0)
+                    {
+                        cout<<"uncleaned is "<<uncleaned<<endl;
                         pthread_mutex_unlock(&unclnlock);
+                        // release lock
                         break;
                     }
-                    --uncleaned;
-                    sem_post(&total_occupancy);
-                    // release lock
-                    pthread_mutex_unlock(&unclnlock);
+                    else
+                    {
+                        if (uncleaned == 1)
+                        {
+                            --uncleaned;
+                            cout<<"uncleaned is "<<uncleaned<<endl;
+                            // release lock
+                            for(int i=0;i<2*N;++i)sem_post(&total_occupancy);
+                            pthread_mutex_unlock(&unclnlock);
+                            break;
+                        }
+                        --uncleaned;
+                        cout<<"uncleaned is "<<uncleaned<<endl;
+                        pthread_mutex_unlock(&unclnlock);
+                    }
+    
+                }
+                else {
+                    pthread_mutex_unlock(&roomlock);
                 }
             }
             else
@@ -289,4 +308,3 @@ int main()
 
     return 0;
 }
-
