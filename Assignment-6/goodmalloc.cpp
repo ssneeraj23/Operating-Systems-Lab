6afb165fedc5c4276bd2ff63a20f59fed14ceade
Neeraj Boddeda <101348975+ssneeraj23@users.noreply.github.com>
2023-04-14 20:19:27 +0530

    Add files via upload

--- Assignment-6/goodmalloc.cpp
+++ Assignment-6/goodmalloc.cpp
@@ -3,80 +3,47 @@
 
 using namespace std;
 
-#define MEM_SIZE 500*1024*1024
-#define PAGE_TABLE_SIZE 1024
+#define MEM_SIZE 500 * 1024 * 1024
+#define PAGE_SIZE 2048             // page size
+#define PAGE_TABLE_SIZE 125 * 1024 // total number of pages in memory
 
-
-#define INT 4
-#define CHAR 1
-#define SHORTINT 2
-
-typedef struct {
+// 170 elements + 8 bytes wastage in a page
+typedef struct
+{
     int prev;
     int data;
     int next;
-}element;
-element* memory;
+} element;
 
-typedef struct{
-    int data_type;  //type of data
-    int type;       //type of symbol is it pointer or variable
-    int size;
-}dataType;
-
-
-typedef struct{
-    int address;
-    //int scope;
-    const char* id;
-    dataType type;
-} symbol;
-
-symbol* STACK;
-vector<symbol> symbol_table;
-
-typedef struct {
-    u_int memory_index : 32;
-    u_int marked : 1; // when it is marked we can allocate it to another variable
-    u_int valid : 1; // valid bit to detect segmentation fault
-}page_table_entry;
-
-vector<page_table_entry> page_table;
-
-int number_of_blocks = MEM_SIZE*1024*1024/sizeof(element);
-
-//The basic idea is to create a stack to remember the scope of the memory  
-// the demilimter between scopes is -1 if free is run without arguments we free until we encounter a -1
-// we will mark the memory so that they are to be freed when the scope is over (mark and sweep)
-// we will use a linked list to keep track of the memory
-// freeElem will have 3 prototypes one for specific address, one for all the scope and one for the whole list
-
-
-int createMemory()
+typedef struct
 {
-    memory = (element*)malloc(MEM_SIZE);
-    if (memory == NULL)
+    string id;
+    vector<int> pages_allocated;
+    // int start_page;
+    // int size;
+} entry;
+
+element *mem_start;
+vector<int> page_table(PAGE_TABLE_SIZE, 1);
+int no_of_free_pages = PAGE_TABLE_SIZE;
+vector<entry> allocation_stack;
+vector<entry> global_stack;
+
+// The basic idea is to create a stack to remember the scope of the memory
+//  the demilimter between scopes is -1 if free is run without arguments we free until we encounter a -1
+//  we will mark the memory so that they are to be freed when the scope is over (mark and sweep)
+//  we will use a linked list to keep track of the memory
+//  freeElem will have 3 prototypes one for specific address, one for all the scope and one for the whole list
+
+int createMemory(int size)
+{
+    // dynamic size to be considered
+    mem_start = (element *)malloc(MEM_SIZE);
+    if (mem_start == NULL)
     {
-        cout << "Error: memory of size" <<MEM_SIZE<<"for memory allocation failed\n" << endl;
+        cout << "Error: memory of size" << MEM_SIZE << "for memory allocation failed\n"
+             << endl;
         return 1;
-    }
-    for(int i = 0; i < number_of_blocks; i++)
-    {
-        memory[i].prev = i-1;
-        memory[i].data = 0;
-        memory[i].next = i+1;
-    }
-    if(PAGE_TABLE_SIZE*1024*1024*sizeof(page_table_entry) > page_table.max_size())
-    {
-        cout << "Error: can't" <<PAGE_TABLE_SIZE*sizeof(page_table_entry)<<"for pagetable allocation failed\n" << endl;
-        return 1;
-    }
-    page_table = vector<page_table_entry>(PAGE_TABLE_SIZE);
-    
-    for(int i = 0; i < PAGE_TABLE_SIZE; i++)
-    {
-        page_table[i].marked = 0;
-        page_table[i].valid = 0;
     }
 
     return 0;
@@ -84,17 +51,187 @@ int createMemory()
 
 void initScope()
 {
-    symbol* temp = new symbol;
-    temp->address = -1;
-    symbol_table.push_back(*temp);
-
+    entry temp;
+    temp.id = "--";
+    temp.pages_allocated.clear();
+    allocation_stack.push_back(temp);
+    return;
 }
 
-int createList(const char* id,int data_type ,ssize_t size){
-
-}
-
-int createVar(int data_type)
+int createList(string name, int size, int isglobal = 0)
 {
+    int pages_needed = (size / 170) + 1;
+    if (pages_needed > no_of_free_pages)
+    {
+        cout << "Error: Not enough memory to allocate" << endl;
+        return -1;
+    }
+    entry temp;
+    temp.id = name;
+    temp.pages_allocated.clear();
+    no_of_free_pages -= pages_needed;
+    for (int i = 0; i < PAGE_TABLE_SIZE; ++i)
+    {
+        if (page_table[i] == 1)
+        {
+            --pages_needed;
+            page_table[i] = 0;
+            temp.pages_allocated.push_back(i);
+            if (pages_needed == 0)
+                break;
+        }
+    }
+    // for(int i=0;i<temp.pages_allocated.size();++i)
+    //{
+    //  for(int offset=0;j<SIZE_OF_PAGE/sizeof(element);++j)
+    //   {
+    //  int index_in_mem = 2048*pages_allocated[i]+offset;
+    //   }
+    // }
+    if (isglobal == 1)
+        global_stack.push_back(temp);
+    else
+        allocation_stack.push_back(temp);
+    return 1; // depicting success
+}
 
+int readVal(string listname, int offset, int isglobal = 0)
+{
+    vector<int> pages;
+    if (isglobal == 1)
+    {
+        for (int x = global_stack.size() - 1; x>=0; x--)
+        {
+            if (global_stack[x].id == listname)
+            {
+                pages = global_stack[x].pages_allocated;
+                break;
+            }
+        }
+
+        if (pages.size() == 0)
+        {
+            cout << "Error: List not found in this scope" << endl;
+            return -1;
+        }
+    }
+    else
+    {
+        for (int x = allocation_stack.size() - 1; allocation_stack[x].id != "--"; x--)
+        {
+            if (allocation_stack[x].id == listname)
+            {
+                pages = allocation_stack[x].pages_allocated;
+                break;
+            }
+        }
+
+        if (pages.size() == 0)
+        {
+            cout << "Error: List not found in this scope" << endl;
+            return -1;
+        }
+    }
+
+    int contained_page = (offset / 170);
+    offset %= 170;
+    contained_page = pages[contained_page];
+    char *start_ad = (char *)mem_start;
+    start_ad = start_ad + (contained_page)*2048;
+    element *page_start = (element *)start_ad;
+    element *req_element = page_start + offset;
+    return req_element->data;
+}
+
+int assignVal(string listname, int offset, int value,int isglobal=0)
+{
+    vector<int> pages;
+
+    if (isglobal == 1)
+    {
+        for (int x = global_stack.size() - 1; x>=0; x--)
+        {
+            if (global[x].id == listname)
+            {
+                pages = global[x].pages_allocated;
+                break;
+            }
+        }
+
+        if (pages.size() == 0)
+        {
+            cout << "Error: List not found in this scope" << endl;
+            return -1;
+        }
+    }
+    else
+    {
+        for (int x = allocation_stack.size() - 1; allocation_stack[x].id != "--"; x--)
+        {
+            if (allocation_stack[x].id == listname)
+            {
+                pages = allocation_stack[x].pages_allocated;
+                break;
+            }
+        }
+
+        if (pages.size() == 0)
+        {
+            cout << "Error: List not found in this scope" << endl;
+            return -1;
+        }
+    }
+
+    int contained_page = (offset / 170);
+    offset %= 170;
+    contained_page = pages[contained_page];
+    char *start_ad = (char *)mem_start;
+    start_ad = start_ad + (contained_page)*2048;
+    element *page_start = (element *)start_ad;
+    element *req_element = page_start + offset;
+    req_element->data = value;
+    return 1; // to indicate sucsess
+}
+
+void freeElem(string id = "?")
+{
+    if (id == "?")
+    {
+        for (int x = allocation_stack.size() - 1; allocation_stack[x].id != "--"; x--)
+        {
+            for (auto y : allocation_stack[x].pages_allocated)
+            {
+                page_table[y] = 1;
+                ++no_of_free_pages;
+            }
+            allocation_stack.pop_back();
+        }
+        allocation_stack.pop_back();
+        if(allocation_stack.size()==0)
+        {
+            for (int x = global_stack.size() - 1; x>=0; x--)
+        {
+            for (auto y : global_stack[x].pages_allocated)
+            {
+                page_table[y] = 1;
+                ++no_of_free_pages;
+            }
+            allocation_stack.pop_back();
+        }
+        }
+        return;
+    }
+    for (int x = allocation_stack.size() - 1; allocation_stack[x].id != "--"; x--)
+    {
+        if (allocation_stack[x].id == id)
+        {
+            for (auto y : allocation_stack[x].pages_allocated)
+            {
+                page_table[y] = 1;
+                ++no_of_free_pages;
+            }
+            allocation_stack.erase(allocation_stack.begin() + x);
+            break;
+        }
+    }
 }
